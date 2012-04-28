dc9ab329f68eccf750dd61af7bbeece5f6e71d13
Tomas Hurban <thagox@gmail.com>
2012-04-28 22:38:13 +0200

    tests - functions export and logging

--- impl/dsearch/src/tests.erl
+++ impl/dsearch/src/tests.erl
@@ -2,7 +2,10 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
+-export([data_providers_search/0, providers_data_search_test/0, update_existing_part/0]).
+
 data_providers_search() ->
+	log("TEST: data_providers_search()"),
 	central_server:start_link(),
 	central_server:update("part1", "abc"),
 	central_server:update("part2", "def"),
@@ -19,9 +22,11 @@ data_providers_search() ->
 	search_provider_supervisor:start_link(),
 	search_provider_supervisor:start_link(),
 	{ok, Results} = central_server:search("g"),
+	log("test end \r\n"),
 	ok.
 
-providers_data_search__test() ->
+providers_data_search_test() ->
+	log("TEST: providers_data_search_test()"),
 	central_server:start_link(),
 	search_provider_supervisor:start_link(),
 	search_provider_supervisor:start_link(),
@@ -31,9 +36,11 @@ providers_data_search__test() ->
 	central_server:update("part1", "abc"),
 	timer:sleep(1000),
 	A = central_server:search("a"),
+	log("test end \r\n"),
 	ok.
 
 update_existing_part() ->
+	log("TEST: update_existing_part()"),
 	central_server:start_link(),
 	central_server:update("part1", "abc"),
 	search_provider_supervisor:start_link(),
@@ -41,4 +48,15 @@ update_existing_part() ->
 	central_server:update("part1", "def"),
 	timer:sleep(2000),
 	{ok, Results} = central_server:search("def"),
+	log("test end \r\n"),
 	ok.
+
+log(What) ->
+	{ok, WriteDescr} = file:open("log.txt", [raw, append]),
+	{Date={Year, Month, Day},Time={Hour, Minutes, Seconds}} = erlang:localtime(),
+	DateTime = io_lib:format('~4..0b-~2..0b-~2..0b ~2..0b:~2..0b:~2..0b', [Year, Month, Day, Hour, Minutes, Seconds]),
+	Module = "tests",
+	Message = "-> " ++ What ++ "\r\n",
+	TextToLog = DateTime ++ " [" ++ Module ++ "] " ++ Message,
+	file:write(WriteDescr, TextToLog),
+	file:close(WriteDescr).
